1f9268d89ba46c329f6963157fde916b9a2b49c0
akilaice <akilaice@gmail.com>
2019-07-31 09:21:50 +0200

    Import options

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
 import * as highcharts from 'highcharts/highcharts';
 import { ChartModule } from 'angular2-highcharts';
-
-import * as grouped from 'highcharts-grouped-categories/grouped-categories';
 import * as exporting from 'highcharts/modules/exporting.src';
+
+
+// import * as grouped from 'highcharts-grouped-categories'; // option 1
+// import * as grouped from 'highcharts-grouped-categories/grouped-categories'; // option 2
+import * as grouped from 'highcharts/highcharts-grouped-categories'; // option 3
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
