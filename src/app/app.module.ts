bc852d57bdd170e9c39d06b8479e479ab9941e63
akilaice <akilaice@gmail.com>
2019-07-31 09:13:54 +0200

    Install dependecies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,46 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
+import * as highcharts from 'highcharts/highcharts';
+import { ChartModule } from 'angular2-highcharts';
+
+import * as grouped from 'highcharts-grouped-categories/grouped-categories';
+import * as exporting from 'highcharts/modules/exporting.src';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+export function highchartsFactory() {
+  // Default options.
+  highcharts.setOptions({
+    global: {
+      useUTC: false
+    },
+    lang: {
+      months: [
+        'Enero', 'Febrero', 'Marzo', 'Abril',
+        'Mayo', 'Junio', 'Julio', 'Agosto',
+        'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+      ],
+      shortMonths: [
+        'Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'
+      ]
+    }
+  });
+
+  return highcharts;
+}
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule ],
+  providers:    [
+    {
+      provide: HighchartsStatic,
+      useFactory: highchartsFactory 
+    }, 
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
